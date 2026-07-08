6ffa120f95c26599b981f6905805f410e89bb91c
Aluno em Vocação Nicolas santana <nicolasdml02@gmail.com>
2026-07-08 16:11:58 -0300

    upgrade_progresso
    progressão das atividades

---
+++ modulo2/modulo2_intro_python_nicolasantana-ativ2.p.py
@@ -0,0 +1,10 @@
+# Atividade 2: Testando comandos simples no Python
+# Explore o interpretador e experimente comandos básicos.
+
+print("Olá, Mundo!")  # Exibe uma mensagem na tela
+
+# Testando o tipo de dados com type()
+print(type("Texto"))  # <class 'str'>
+print(type(42))       # <class 'int'>
+print(type(3.14))     # <class 'float'>
+print(type(True))     # <class 'bool'>
